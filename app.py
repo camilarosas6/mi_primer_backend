1e6ac1dd9e6a21d7b6cc837b447407c12cebf2f7
camilarosas6 <camila.rosass@outlook.com>
2024-11-11 13:20:22 -0600

    referencias

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+
+usuario = {
+    'email': 'ejemplo@gmail.com',
+    'password' : '123456'
+}
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+@app.route('/habilidades')
+def habilidades():
+    return render_template('habilidades.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+@app.route('/success', methods=['post'])
+def success():
+    email = request.form['email']
+    password = request.form['password']
+    print (email, password)
+    if email == usuario['email'] and password == usuario['password']:
+        return render_template('success.html')
+    
+    else:
+        print("no :( )")
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
